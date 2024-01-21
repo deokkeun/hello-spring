ee1a1670f833b1c3561cbd43c9fb95e8f19bff1a
김덕근 <deokk91@gmail.com>
2024-01-21 21:02:56 +0900

    #2 - 정적 컨텐츠, MVC와 템플릿 엔진, API, ResponseBody 사용 원리

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -53,8 +53,8 @@ public class HelloController {
     // byte 처리 등등 기타 여러 HttpMessageConverter가 기본으로 등록되어 있음
 
     // 참고: 클라이언트의 HTTP Accept 헤더와 서버의 컨트롤러 반환 타입 정보 둘을 조합해서
-
     // `HttpMessageConverter`가 선택된다.
+    
     @GetMapping("hello-api")
     @ResponseBody
     public Hello helloApi(@RequestParam("name") String name) {
